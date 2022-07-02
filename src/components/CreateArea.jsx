2c58bf69779600bee59826eb0929b109977b19b9
favydabs <favourdabit@gmail.com>
2022-07-02 10:20:41 +0100

    complete stage

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
@@ -15,14 +15,27 @@ function CreateArea() {
       };
     });
   }
+  function submitNote(event) {
+    props.onAdd(note)
+    event.preventDefault();
+  }
 
 
   return (
     <div>
       <form>
-        <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />
-        <textarea onChange={handleChange} name="content" value={note.content} placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          onChange={handleChange}
+          name="title"
+          value={note.title}
+          placeholder="Title" />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3" />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
